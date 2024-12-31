04ba94e58e4fc37f803d65e1dbd48b64b745e136
ProjetosEltonLira <eltondelira@gmail.com>
2024-12-31 20:12:01 -0300

    feat: Inclusao do FeignClients, exemplo de execucao

--- src/main/kotlin/com/portifolio/smartStock/SmartStockApplication.kt
+++ src/main/kotlin/com/portifolio/smartStock/SmartStockApplication.kt
@@ -1,11 +1,22 @@
 package com.portifolio.smartStock
 
+import com.portifolio.smartStock.client.GoogleClient
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-class SmartStockApplication
+@EnableFeignClients
+class SmartStockApplication (private var googleClient: GoogleClient) : CommandLineRunner {
+
+
+	override fun run(vararg args: String?) {
+		googleClient.helloGoogle()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SmartStockApplication>(*args)
 }
+
